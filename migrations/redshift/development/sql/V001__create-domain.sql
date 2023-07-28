9aea2513730600dae65acd154f957e58f7fc0f7a
Hari Chintala <hari.chintala@digital.justice.gov.uk>
2023-07-28 11:21:02 +0100

    Add External Schema Creation:

--- migrations/redshift/development/sql/V001__create-domain.sql
+++ migrations/redshift/development/sql/V001__create-domain.sql
@@ -1,4 +1,4 @@
 CREATE EXTERNAL SCHEMA domain from data catalog
 database 'domain'
-iam_role 'arn:aws:iam::<account>:role/dpr-redshift-spectrum-role'
+iam_role 'arn:aws:iam::771283872747:role/dpr-redshift-spectrum-role'
 create external database if not exists;
