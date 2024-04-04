50ede2e6e2c9257a6007464f6f5255a1bfe9f8d8
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2024-04-04 15:29:11 +0100

    DPR2-511: Drop domain schema before creation

--- migrations/development/redshift/sql/V007__recreate-schema-domain.sql
+++ migrations/development/redshift/sql/V007__recreate-schema-domain.sql
@@ -1,3 +1,5 @@
+DROP SCHEMA IF EXISTS domain CASCADE;
+
 CREATE EXTERNAL SCHEMA IF NOT EXISTS domain from data catalog
 database 'domain'
 iam_role 'arn:aws:iam::771283872747:role/dpr-redshift-spectrum-role'
