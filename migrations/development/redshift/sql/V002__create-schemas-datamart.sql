93c948e8aefe45d44e461136f184022b44e3a8a0
Kolade Adewuyi <kolade.adewuyi@digital.justice.gov.uk>
2024-04-09 16:40:54 +0100

    DPR2-511: Restore subsequent migrations

---
+++ migrations/development/redshift/sql/V002__create-schemas-datamart.sql
@@ -0,0 +1,26 @@
+-- =================================================================
+-- Create a Spectrum link to the curated layer via the prisons 
+-- hive catalogue
+--
+-- As each new table is deployed, this will
+-- be automatically refreshed.
+--
+-- =================================================================
+CREATE EXTERNAL SCHEMA IF NOT EXISTS prisons from data catalog
+database 'prisons'
+iam_role 'arn:aws:iam::771283872747:role/dpr-redshift-spectrum-role';
+
+
+-- =================================================================
+-- Drop the schema if exists
+-- =================================================================
+DROP SCHEMA IF EXISTS datamart CASCADE;
+
+-- =================================================================
+-- Create a schema space for datamart
+-- All datamart views will be deployed here on a domain_table basis
+--
+-- =================================================================
+
+CREATE SCHEMA IF NOT EXISTS datamart QUOTA UNLIMITED;
+GRANT USAGE ON SCHEMA datamart TO dpruser;
---
+++ migrations/development/redshift/sql/V004__functions.sql
@@ -0,0 +1,20 @@
+-- =================================================================
+-- Function age
+-- input : $1 date
+-- output : age of person
+--
+-- uses stable so can be re-used for the same statement
+-- =================================================================
+CREATE OR REPLACE FUNCTION datamart.age(date)
+    returns bigint
+stable
+AS $$
+    select DATEDIFF(hour,$1,CURRENT_DATE)/8766
+$$ language sql;
+
+CREATE OR REPLACE FUNCTION datamart.age(TIMESTAMP WITHOUT TIME ZONE)
+    returns bigint
+    stable
+AS $$
+    select DATEDIFF(hour,$1,CURRENT_DATE)/8766
+$$ language sql;
