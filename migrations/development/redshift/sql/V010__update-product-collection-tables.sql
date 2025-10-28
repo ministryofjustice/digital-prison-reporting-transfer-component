b669c5e750ddaf0dec83d88a0dfb8fa219b14d79
James Reed <james.reed1@justice.gov.uk>
2025-10-28 16:17:19 +0000

    drop and recreate so we dont need defaults

--- migrations/development/redshift/sql/V010__update-product-collection-tables.sql
+++ migrations/development/redshift/sql/V010__update-product-collection-tables.sql
@@ -1,5 +1,13 @@
-ALTER TABLE product_.product_collection ADD COLUMN owner_name VARCHAR NOT NULL;
-ALTER TABLE product_.product_collection ADD COLUMN version VARCHAR NOT NULL,
+DROP TABLE product_.product_collection;
+
+CREATE TABLE product_.product_collection (
+  id VARCHAR NOT NULL,
+  name VARCHAR NOT NULL,
+  version VARCHAR NOT NULL,
+  owner_name VARCHAR NOT NULL,
+  products SUPER NOT NULL, -- Caching layer for product_collection_products that will be updated whenever product_collection_products is updated
+  PRIMARY KEY(id)
+);
 
 CREATE TABLE product_.product_collection_attributes (
   product_collection_id VARCHAR SORTKEY NOT NULL,
