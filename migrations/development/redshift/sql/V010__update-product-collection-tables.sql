68ed01ff3239f7020a49e605c870d105eb9a74cb
James Reed <james.reed1@justice.gov.uk>
2025-10-28 16:12:26 +0000

    alter not update

--- migrations/development/redshift/sql/V010__update-product-collection-tables.sql
+++ migrations/development/redshift/sql/V010__update-product-collection-tables.sql
@@ -1,5 +1,5 @@
-UPDATE TABLE product_.product_collection ADD COLUMN owner_name VARCHAR NOT NULL;
-UPDATE TABLE product_.product_collection ADD COLUMN version VARCHAR NOT NULL,
+ALTER TABLE product_.product_collection ADD COLUMN owner_name VARCHAR NOT NULL;
+ALTER TABLE product_.product_collection ADD COLUMN version VARCHAR NOT NULL,
 
 CREATE TABLE product_.product_collection_attributes (
   product_collection_id VARCHAR SORTKEY NOT NULL,
