f23512a3d248297649ab941b4f92a14c568d46a8
James Reed <james.reed1@justice.gov.uk>
2025-10-28 13:26:58 +0000

    use correct syntax

--- migrations/development/redshift/sql/V009__create-product-collection-tables.sql
+++ migrations/development/redshift/sql/V009__create-product-collection-tables.sql
@@ -1,11 +1,10 @@
-USE SCHEMA product_;
-CREATE TABLE product_collection (
+CREATE TABLE product_.product_collection (
   id VARCHAR NOT NULL,
   name VARCHAR NOT NULL,
   products SUPER NOT NULL, -- Caching layer for product_collection_products that will be updated whenever product_collection_products is updated
   PRIMARY KEY(id)
 );
-CREATE TABLE product_collection_products (
+CREATE TABLE product_.product_collection_products (
   product_collection_id VARCHAR NOT NULL,
   product_id VARCHAR NOT NULL,
   FOREIGN KEY(product_collection_id) REFERENCES product_collection(id),
