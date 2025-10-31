0232968cc5eeb5260538f59397217db94b7f849c
Tom Ogle <tom.ogle@justice.gov.uk>
2025-10-31 11:56:48 +0000

    DHS-497: Fix table name.

--- migrations/test/operationaldatastore/sql/V012__add_internal_movement_allowed_col_to_locationsinsideprison_location.sql
+++ migrations/test/operationaldatastore/sql/V012__add_internal_movement_allowed_col_to_locationsinsideprison_location.sql
@@ -1,1 +1,1 @@
-ALTER TABLE prisons.locationsinsideprison ADD COLUMN internal_movement_allowed boolean;
+ALTER TABLE prisons.locationsinsideprison_location ADD COLUMN internal_movement_allowed boolean;
