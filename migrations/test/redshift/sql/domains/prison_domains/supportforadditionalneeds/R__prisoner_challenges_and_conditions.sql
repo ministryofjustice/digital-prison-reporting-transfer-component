ada7bbcb47ae62eff138a93bf4fe276aea381d0d
Tom Ogle <tom.ogle@justice.gov.uk>
2026-01-15 15:06:57 +0000

    DHS-585 and DHS-577: Promote domain views through environments.

--- migrations/test/redshift/sql/domains/prison_domains/supportforadditionalneeds/R__prisoner_challenges_and_conditions.sql
+++ migrations/test/redshift/sql/domains/prison_domains/supportforadditionalneeds/R__prisoner_challenges_and_conditions.sql
@@ -1,16 +1,19 @@
+/* 13/01/2025 Version 2 replace 1/0 and include N/A instead of NULLS for has_need, has_aln_need, has_ldd_need columns.
+ * cell location and ethnicity description
+ */
 CREATE OR REPLACE VIEW prison_domains.supportforadditionalneeds_prisoner_challenges_and_conditions AS
 WITH base AS (SELECT cp.prisoner_number AS prison_number,
                      cp.prison_id       AS current_location,
-                     cp.sex_code,
-                     cp.first_name,
-                     cp.last_name,
-                     cp.date_of_birth   as birth_date,
+                     cp.gender_code     as sex_code,
+                     cp.age,
+                     cp.cell_location,
+                     cp.ethnicity_code,
+                     cp.ethnicity,
                      ov.has_need,
                      ov.has_aln_need,
                      ov.has_ldd_need
-              FROM datamart.prisoner.profile AS cp
-                       LEFT JOIN datamart.prison_domains."supportforadditionalneeds_prisoner_overview" ov
-                                 ON cp.prisoner_number = ov.prison_number
+              FROM datamart.prison_domains."supportforadditionalneeds_prisoner_overview" ov
+                       LEFT JOIN datamart.person.prisoner cp ON ov.prison_number = cp.prisoner_number
               WHERE cp.prison_id NOT in ('OUT', 'ZZGHI')),
      ref AS (SELECT r.id   AS condition_id,
                     r.code AS condition_code
@@ -44,92 +47,102 @@ WITH base AS (SELECT cp.prisoner_number AS prison_number,
 --      AND ch.__current = TRUE
      ),
      conditions_pivot AS (SELECT c.prison_number,
-                                 -- max updated to work with integer
-                                 MAX(CASE WHEN r.condition_code = 'PHYSICAL_OTHER' THEN TRUE::INTEGER END) AS conditions_restricting_mobility_dexterity,
-                                 MAX(CASE WHEN r.condition_code = 'DLD_OTHER' THEN 1 END)                  AS other_language_speech_communication_disorder,
-                                 MAX(CASE WHEN r.condition_code = 'DYSPRAXIA' THEN 1 END)                  AS dyspraxia_developmental_coordination_disorder,
-                                 MAX(CASE WHEN r.condition_code = 'ABI' THEN 1 END)                        AS acquired_brain_injury,
-                                 MAX(CASE WHEN r.condition_code = 'DYSLEXIA' THEN 1 END)                   AS dyslexia,
-                                 MAX(CASE WHEN r.condition_code = 'ASC' THEN 1 END)                        AS autism_spectrum_condition,
-                                 MAX(CASE WHEN r.condition_code = 'ADHD' THEN 1 END)                       AS attention_deficit_hyperactivity_disorder_adhd_add,
-                                 MAX(CASE WHEN r.condition_code = 'DYSCALCULIA' THEN 1 END)                AS dyscalculia,
-                                 MAX(CASE WHEN r.condition_code = 'LD_OTHER' THEN 1 END)                   AS other_learning_disability,
-                                 MAX(CASE WHEN r.condition_code = 'MENTAL_HEALTH' THEN 1 END)              AS mental_health,
-                                 MAX(CASE WHEN r.condition_code = 'VISUAL_IMPAIR' THEN 1 END)              AS visual_impairment,
-                                 MAX(CASE WHEN r.condition_code = 'OTHER' THEN 1 END)                      AS other_disability_or_health_condition_not_listed_above,
-                                 MAX(CASE WHEN r.condition_code = 'LEARN_DIFF_OTHER' THEN 1 END)           AS other,
-                                 MAX(CASE WHEN r.condition_code = 'LD_DOWN' THEN 1 END)                    AS downs_syndrome,
-                                 MAX(CASE WHEN r.condition_code = 'DLD_LANG' THEN 1 END)                   AS developmental_language_disorder,
-                                 MAX(CASE WHEN r.condition_code = 'NEURODEGEN' THEN 1 END)                 AS neurodegenerative_condition,
-                                 MAX(CASE WHEN r.condition_code = 'FASD' THEN 1 END)                       AS foetal_alcohol_spectrum_disorder,
-                                 MAX(CASE WHEN r.condition_code = 'DLD_HEAR' THEN 1 END)                   AS hearing_impairment,
-                                 MAX(CASE WHEN r.condition_code = 'TOURETTES' THEN 1 END)                  AS tourettes_syndrome_tic_disorder,
-                                 MAX(CASE WHEN r.condition_code = 'LONG_TERM_OTHER' THEN 1 END)            AS other_long_term_medical_condition,
-                                 MAX(CASE WHEN r.condition_code = 'NEURO_OTHER' THEN 1 END)                AS other_neurological_condition
+                                 -- max updated to work with booleans
+                                 BOOL_OR(CASE WHEN r.condition_code = 'PHYSICAL_OTHER' THEN TRUE END)   AS conditions_restricting_mobility_dexterity,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'DLD_OTHER' THEN TRUE END)        AS other_language_speech_communication_disorder,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'DYSPRAXIA' THEN TRUE END)        AS dyspraxia_developmental_coordination_disorder,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'ABI' THEN TRUE END)              AS acquired_brain_injury,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'DYSLEXIA' THEN TRUE END)         AS dyslexia,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'ASC' THEN TRUE END)              AS autism_spectrum_condition,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'ADHD' THEN TRUE END)             AS attention_deficit_hyperactivity_disorder_adhd_add,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'DYSCALCULIA' THEN TRUE END)      AS dyscalculia,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'LD_OTHER' THEN TRUE END)         AS other_learning_disability,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'MENTAL_HEALTH' THEN TRUE END)    AS mental_health,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'VISUAL_IMPAIR' THEN TRUE END)    AS visual_impairment,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'OTHER' THEN TRUE END)            AS other_disability_or_health_condition_not_listed_above,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'LEARN_DIFF_OTHER' THEN TRUE END) AS other,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'LD_DOWN' THEN TRUE END)          AS downs_syndrome,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'DLD_LANG' THEN TRUE END)         AS developmental_language_disorder,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'NEURODEGEN' THEN TRUE END)       AS neurodegenerative_condition,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'FASD' THEN TRUE END)             AS foetal_alcohol_spectrum_disorder,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'DLD_HEAR' THEN TRUE END)         AS hearing_impairment,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'TOURETTES' THEN TRUE END)        AS tourettes_syndrome_tic_disorder,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'LONG_TERM_OTHER' THEN TRUE END)  AS other_long_term_medical_condition,
+                                 BOOL_OR(CASE WHEN r.condition_code = 'NEURO_OTHER' THEN TRUE END)      AS other_neurological_condition
                           FROM cond c
                                    JOIN ref r
                                         ON c.condition_type_id = r.condition_id
                           GROUP BY c.prison_number),
      challenges_pivot AS (SELECT ch.prison_number,
                                  -- max updated to work with integer
-                                 MAX(CASE WHEN cr.challenge_group = 'PHYSICAL_SKILLS' THEN 1 END)           AS challenge_physical_skills,
-                                 MAX(CASE WHEN cr.challenge_group = 'ATTENTION_ORGANISING_TIME' THEN 1 END) AS challenge_attention_organising_time,
-                                 MAX(CASE WHEN cr.challenge_group = 'PROCESSING_SPEED' THEN 1 END)          AS challenge_processing_speed,
-                                 MAX(CASE WHEN cr.challenge_group = 'NUMERACY_SKILLS' THEN 1 END)           AS challenge_numeracy_skills,
-                                 MAX(CASE WHEN cr.challenge_group = 'SENSORY' THEN 1 END)                   AS challenge_sensory,
-                                 MAX(CASE WHEN cr.challenge_group = 'LANGUAGE_COMM_SKILLS' THEN 1 END)      AS challenge_language_comm_skills,
-                                 MAX(CASE WHEN cr.challenge_group = 'EMOTIONS_FEELINGS' THEN 1 END)         AS challenge_emotions_feelings,
-                                 MAX(CASE WHEN cr.challenge_group = 'LITERACY_SKILLS' THEN 1 END)           AS challenge_literacy_skills,
-                                 MAX(CASE WHEN cr.challenge_group = 'MEMORY' THEN 1 END)                    AS challenge_memory
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'PHYSICAL_SKILLS' THEN TRUE END)           AS challenge_physical_skills,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'ATTENTION_ORGANISING_TIME' THEN TRUE END) AS challenge_attention_organising_time,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'PROCESSING_SPEED' THEN TRUE END)          AS challenge_processing_speed,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'NUMERACY_SKILLS' THEN TRUE END)           AS challenge_numeracy_skills,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'SENSORY' THEN TRUE END)                   AS challenge_sensory,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'LANGUAGE_COMM_SKILLS' THEN TRUE END)      AS challenge_language_comm_skills,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'EMOTIONS_FEELINGS' THEN TRUE END)         AS challenge_emotions_feelings,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'LITERACY_SKILLS' THEN TRUE END)           AS challenge_literacy_skills,
+                                 BOOL_OR(CASE WHEN cr.challenge_group = 'MEMORY' THEN TRUE END)                    AS challenge_memory
                           FROM challenge ch
                                    JOIN challenge_ref cr
                                         ON ch.challenge_type_id = cr.challenge_type_id
                           GROUP BY ch.prison_number)
 SELECT b.prison_number,
-       b.first_name,
-       b.last_name,
-       b.birth_date,
+       b.age,
        b.sex_code,
        b.current_location,
+       b.cell_location,
+       b.ethnicity_code,
+       b.ethnicity,
        b.has_need,
-       b.has_aln_need,
-       b.has_ldd_need,
+       CASE
+           WHEN b.has_aln_need IS NULL THEN 'N/A'
+           WHEN b.has_aln_need THEN 'true'
+           ELSE 'false'
+           END                                                                    AS has_aln_need,
+       CASE
+           WHEN b.has_ldd_need IS NULL THEN 'N/A'
+           WHEN b.has_ldd_need THEN 'true'
+           ELSE 'false'
+           END                                                                    AS has_ldd_need,
        -- Conditions
-       c.conditions_restricting_mobility_dexterity,
-       c.other_language_speech_communication_disorder,
-       c.dyspraxia_developmental_coordination_disorder,
-       c.acquired_brain_injury,
-       c.dyslexia,
-       c.autism_spectrum_condition,
-       c.attention_deficit_hyperactivity_disorder_adhd_add,
-       c.dyscalculia,
-       c.other_learning_disability,
-       c.mental_health,
-       c.visual_impairment,
-       c.other_disability_or_health_condition_not_listed_above,
-       c.other,
-       c.downs_syndrome,
-       c.developmental_language_disorder,
-       c.neurodegenerative_condition,
-       c.foetal_alcohol_spectrum_disorder,
-       c.hearing_impairment,
-       c.tourettes_syndrome_tic_disorder,
-       c.other_long_term_medical_condition,
-       c.other_neurological_condition,
-       -- Challenges
-       ch.challenge_physical_skills,
-       ch.challenge_attention_organising_time,
-       ch.challenge_processing_speed,
-       ch.challenge_numeracy_skills,
-       ch.challenge_sensory,
-       ch.challenge_language_comm_skills,
-       ch.challenge_emotions_feelings,
-       ch.challenge_literacy_skills,
-       ch.challenge_memory
+       COALESCE(c.conditions_restricting_mobility_dexterity, 'false')             AS conditions_restricting_mobility_dexterity,
+       COALESCE(c.other_language_speech_communication_disorder, 'false')          AS other_language_speech_communication_disorder,
+       COALESCE(c.dyspraxia_developmental_coordination_disorder, 'false')         AS dyspraxia_developmental_coordination_disorder,
+       COALESCE(c.acquired_brain_injury, 'false')                                 AS acquired_brain_injury,
+       COALESCE(c.dyslexia, 'false')                                              AS dyslexia,
+       COALESCE(c.autism_spectrum_condition, 'false')                             AS autism_spectrum_condition,
+       COALESCE(c.attention_deficit_hyperactivity_disorder_adhd_add,
+                'false')                                                          AS attention_deficit_hyperactivity_disorder_adhd_add,
+       COALESCE(c.dyscalculia, 'false')                                           AS dyscalculia,
+       COALESCE(c.other_learning_disability, 'false')                             AS other_learning_disability,
+       COALESCE(c.mental_health, 'false')                                         AS mental_health,
+       COALESCE(c.visual_impairment, 'false')                                     AS visual_impairment,
+       COALESCE(c.other_disability_or_health_condition_not_listed_above,
+                'false')                                                          AS other_disability_or_health_condition_not_listed_above,
+       COALESCE(c.other, 'false')                                                 AS other,
+       COALESCE(c.downs_syndrome, 'false')                                        AS downs_syndrome,
+       COALESCE(c.developmental_language_disorder, 'false')                       AS developmental_language_disorder,
+       COALESCE(c.neurodegenerative_condition, 'false')                           AS neurodegenerative_condition,
+       COALESCE(c.foetal_alcohol_spectrum_disorder, 'false')                      AS foetal_alcohol_spectrum_disorder,
+       COALESCE(c.hearing_impairment, 'false')                                    AS hearing_impairment,
+       COALESCE(c.tourettes_syndrome_tic_disorder, 'false')                       AS tourettes_syndrome_tic_disorder,
+       COALESCE(c.other_long_term_medical_condition, 'false')                     AS other_long_term_medical_condition,
+       COALESCE(c.other_neurological_condition, 'false')                          AS other_neurological_condition,
+       COALESCE(ch.challenge_physical_skills, 'false')                            AS challenge_physical_skills,
+       COALESCE(ch.challenge_attention_organising_time, 'false')                  AS challenge_attention_organising_time,
+       COALESCE(ch.challenge_processing_speed, 'false')                           AS challenge_processing_speed,
+       COALESCE(ch.challenge_numeracy_skills, 'false')                            AS challenge_numeracy_skills,
+       COALESCE(ch.challenge_sensory, 'false')                                    AS challenge_sensory,
+       COALESCE(ch.challenge_language_comm_skills, 'false')                       AS challenge_language_comm_skills,
+       COALESCE(ch.challenge_emotions_feelings, 'false')                          AS challenge_emotions_feelings,
+       COALESCE(ch.challenge_literacy_skills, 'false')                            AS challenge_literacy_skills,
+       COALESCE(ch.challenge_memory, 'false')                                     AS challenge_memory
 FROM base b
          LEFT JOIN conditions_pivot c
                    ON b.prison_number = c.prison_number
          LEFT JOIN challenges_pivot ch
-                   ON b.prison_number = ch.prison_number
+                   ON ch.prison_number = b.prison_number
 ORDER BY b.current_location, b.prison_number
 WITH NO SCHEMA BINDING;
