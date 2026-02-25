f7b14f9a19ba1cece717082251157c791927ae2a
James Reed <james.reed1@justice.gov.uk>
2026-02-25 10:26:22 +0000

    didnt have id col

--- migrations/development/redshift/sql/V015__add-product-collection-data-activities.sql
+++ migrations/development/redshift/sql/V015__add-product-collection-data-activities.sql
@@ -1,5 +1,5 @@
 INSERT INTO
-  product_.product_collection (name, version, owner_name)
+  product_.product_collection (id, name, version, owner_name)
 VALUES
   (
     'cc5ba063-f5ce-42d1-a776-52a9249e6ed5',
