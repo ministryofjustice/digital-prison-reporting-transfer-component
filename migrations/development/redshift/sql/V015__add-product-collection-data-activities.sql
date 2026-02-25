b8a291419af20680143cb8cfa1f7a5056de9a5f8
James Reed <james.reed1@justice.gov.uk>
2026-02-25 10:16:10 +0000

    add new activities product collection data

---
+++ migrations/development/redshift/sql/V015__add-product-collection-data-activities.sql
@@ -0,0 +1,33 @@
+INSERT INTO
+  product_.product_collection (name, version, owner_name)
+VALUES
+  (
+    'cc5ba063-f5ce-42d1-a776-52a9249e6ed5',
+    'Activities',
+    '1.0',
+    'DPR'
+  );
+
+INSERT INTO
+  product_.product_collection_products (product_collection_id, product_id)
+VALUES
+  (
+    'cc5ba063-f5ce-42d1-a776-52a9249e6ed5',
+    'act002-waitlist-applications-by-caseload'
+  );
+
+INSERT INTO
+  product_.product_collection_products (product_collection_id, product_id)
+VALUES
+  (
+    'cc5ba063-f5ce-42d1-a776-52a9249e6ed5',
+    'act003-waitlist-average-by-caseload'
+  );
+
+INSERT INTO
+  product_.product_collection_products (product_collection_id, product_id)
+VALUES
+  (
+    'cc5ba063-f5ce-42d1-a776-52a9249e6ed5',
+    'act004-waitlist-average-by-establishment'
+  );
