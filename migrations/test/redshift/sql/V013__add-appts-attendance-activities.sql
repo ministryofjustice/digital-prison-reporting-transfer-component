b1efe4d9486642a23366a3cef85ccef9cb623030
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2026-04-15 14:18:36 +0100

    add script to all other envs

---
+++ migrations/test/redshift/sql/V013__add-appts-attendance-activities.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+  product_.product_collection_products (product_collection_id, product_id)
+VALUES
+  (
+    'cc5ba063-f5ce-42d1-a776-52a9249e6ed5',
+    'act006-appointment-attendance-by-caseload'
+  );
