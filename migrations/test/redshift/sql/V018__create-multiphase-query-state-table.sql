2df1ce80c5d1d1c965c4a7100f1f8cb7d7d9ed38
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2026-06-29 15:36:45 +0100

    PDHD-89 added admin schema and multiphase_query_state table as they did not exist in this project but are present in Redshift up to preprod

---
+++ migrations/test/redshift/sql/V018__create-multiphase-query-state-table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE admin.multiphase_query_state (
+                                              root_execution_id character varying(256) NOT NULL ENCODE lzo COLLATE case_sensitive,
+                                              current_execution_id character varying(256) ENCODE lzo COLLATE case_sensitive,
+                                              datasource_name character varying(256) ENCODE lzo COLLATE case_sensitive,
+                                              catalog character varying(256) ENCODE lzo COLLATE case_sensitive,
+                                              database character varying(256) ENCODE lzo COLLATE case_sensitive,
+                                              index integer ENCODE az64,
+                                              current_state character varying(256) ENCODE lzo COLLATE case_sensitive,
+                                              query super COLLATE case_sensitive,
+                                              error super COLLATE case_sensitive,
+                                              sequence_number integer ENCODE az64,
+                                              last_update timestamp without time zone ENCODE az64,
+                                              PRIMARY KEY (root_execution_id)
+) DISTSTYLE AUTO;
