eaee9b3954a2bbb5b8dbd59bd793bc6d297fdf00
gavriil.gavriilidis <gavriil.gavriilidis@digital.justice.gov.uk>
2026-06-29 15:40:03 +0100

    PDHD-89 added if not exists to the table creation

--- migrations/production/redshift/sql/V018__create-multiphase-query-state-table.sql
+++ migrations/production/redshift/sql/V018__create-multiphase-query-state-table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE admin.multiphase_query_state (
+CREATE TABLE IF NOT EXISTS admin.multiphase_query_state (
                                               root_execution_id character varying(256) NOT NULL ENCODE lzo COLLATE case_sensitive,
                                               current_execution_id character varying(256) ENCODE lzo COLLATE case_sensitive,
                                               datasource_name character varying(256) ENCODE lzo COLLATE case_sensitive,
