3e2fbcb3719f8d88b7c222babdd6ff343bb1dc0b
djcostin <daniel.costin@gmail.com>
2026-08-07 11:12:40 +0100

    DPR2-3048 add user subscription table

---
+++ migrations/development/redshift/sql/V025__create-user-subscriptions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE
+  subscription_.user_subscription (
+    id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    report_id TEXT NOT NULL,
+    report_variant_id TEXT NOT NULL,
+    status TEXT NOT NULL,
+    created_time TIMESTAMP DEFAULT SYSDATE,
+    updated_time TIMESTAMP
+  ) DISTSTYLE KEY DISTKEY(id);
